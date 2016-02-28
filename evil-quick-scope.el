d1cf8af92e1ad3373658d7b2919d3a922e88aad9
xd1le <elisp.vim@gmail.com>
2016-02-28 18:43:55 +1100

    docs: add proper comment header
    Complete with documentation and license boileplate.

--- evil-quick-scope.el
+++ evil-quick-scope.el
@@ -1,15 +1,165 @@
 ;;; evil-quick-scope.el --- Lightning fast f/F/t/T movement for Evil mode.
-;;
+
+;; Copyright (C) 2016 by xd1le
+
 ;; Author: xd1le
+;; Maintainer: xd1le
 ;; Version: 0.1.0
 ;; Created: 15th August 2015
 ;; Keywords: highlight, character, evil, convenience
-;; URL: https://gitlab.com/fi/evil-quick-scope
+;; URL: https://github.com/vyp/evil-quick-scope
 ;; Package-Requires: ((evil "1.2.2") (ov "1.0.6"))
-;;;
 
-;; TODO: Add a proper comment header, complete with documentation and license
-;; boilerplate.
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program as a file named `copying` in the root of this repository. If
+;; not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Minor mode that attempts to emulate Brian Le's [1] quick-scope vim plugin [2]
+;; for Evil mode [3].
+;;
+;; [1]: https://plus.google.com/102336503306134343850
+;; [2]: https://github.com/unblevable/quick-scope
+;; [3]: https://bitbucket.org/lyro/evil
+
+;;; Introduction:
+
+;; Find yourself repeatedly but cautiously pressing `;`/`,` often to get to
+;; where you want? Quick scope for Evil mode highlights the characters you can
+;; reach via `f`/`F` with two or three keystrokes, at most once per word.
+;;
+;; It does not provide any commands or keybindings, it simply augments the only
+;; drawback of the `f`/`F`/`t`/`T` motions: it is difficult and slow to
+;; consistently choose the best characters to target.
+;;
+;; Quick scope does the counting for you and presents you with the optimal
+;; choices, resulting in faster and *more accurate* "character motions", as
+;; Brian likes to call them.
+;;
+;; To avoid too much visual clutter, quick scope ignores "special" characters
+;; (i.e. punctuation and similar) that tend to only appear once or twice per
+;; line, and are therefore usually easy to get to.
+;;
+;; Quick scope attempts to highlight closest to the beginning of each word if
+;; possible.
+;;
+;; A "word" separator is defined as any of the following characters:
+;;
+;; - Whitespace.
+;; - Hyphen ("-").
+;; - Underscore ("_").
+;; - Forward slash ("/").
+;; - Period (".").
+
+;;; Configuration:
+
+;; Enabling Minor Mode At Startup:
+;; ===============================
+;;
+;; Because quick scope is a minor mode, you can just run the
+;; `evil-quick-scope-mode` command to toggle it on or off for the current
+;; buffer. Pass `t` to unconditionally turn on, or pass `-1` to unconditionally
+;; turn off.
+;;
+;; Therefore, for example, to turn on quick scope for all text and programming
+;; modes, simply add the appropriate hooks (in your `.emacs` or `init.el`):
+;;
+;;     (require 'evil-quick-scope)
+;;
+;;     (add-hook 'text-mode-hook 'evil-quick-scope-mode)
+;;     (add-hook 'prog-mode-hook 'evil-quick-scope-mode)
+;;
+;; But now say you do not want it enabled in a particular text mode, say LaTeX
+;; mode, you can disable it specifically for LaTeX mode by passing `-1` as said
+;; above:
+;;
+;;     (add-hook 'latex-mode-hook (lambda () (evil-quick-scope-mode -1)))
+;;
+;; If you find yourself wanting it enabled nearly everywhere except for a few
+;; modes, it may be easier to take a 'blacklisting' approach by defining and
+;; enabling a globalized minor mode:
+;;
+;;     (defun turn-on-evil-quick-scope-mode ()
+;;       "Unconditionally turn on evil-quick-scope-mode."
+;;       (evil-quick-scope-mode t))
+;;
+;;     (define-globalized-minor-mode my-global-evil-quick-scope-mode
+;;       evil-quick-scope-mode turn-on-evil-quick-scope-mode)
+;;
+;;     (my-global-evil-quick-scope-mode t)
+;;
+;; Then blacklist with each mode's hook that you want disabled using the example
+;; shown above with LaTeX mode.
+;;
+;; Only Highlight After Keypress:
+;; ==============================
+;;
+;; If you find keeping the highlighting always on too annoying/intrusive, it may
+;; be a better option to only enable highlighting after pressing the
+;; `f`/`F`/`t`/`T` keys:
+;;
+;;     (setq-default evil-qs-highlight-on-keypress '("f" "F" "t" "T"))
+;;
+;; By default this option is `nil`, hence why highlighting is always on by
+;; default.
+;;
+;; It takes a list of four key sequences [4]. The list items correspond to the
+;; following functionality respectively:
+;;
+;; 1. `evil-find-char`
+;; 2. `evil-find-char-backward`
+;; 3. `evil-find-char-to`
+;; 4. `evil-find-char-to-backward`
+;;
+;; These key sequences are then used to make mappings for the normal, visual,
+;; operator and motion evil states.
+;;
+;; [4]: http://www.gnu.org/software/emacs/manual/html_node/elisp/Key-Sequences.html
+;;
+;; Appearance:
+;; ===========
+;;
+;; There are four faces that can be customised to determine the appearance of
+;; the highlighting:
+;;
+;; 1. `evil-qs-forward-primary`
+;; 2. `evil-qs-forward-secondary`
+;; 3. `evil-qs-backward-primary`
+;; 4. `evil-qs-backward-secondary`
+;;
+;; Should be self-explanatory with the following:
+;;
+;; - "**Primary**" refers to characters that can be reached in **two**
+;;   keystrokes just by pressing `f`/`F` and then the character.
+;;
+;;     - By default these faces are given the "green" foreground color.
+;;
+;; - "**Secondary**" refers to characters that require additionally pressing a
+;;   semicolon (`;`) to be reached, hence requiring **three** keystrokes.
+;;
+;;     - By default these faces are given the "blue" foreground color.
+;;
+;; - "Forward" refers to characters highlighted *after* the cursor,
+;;   i.e. navigated via `f`.
+;;
+;; - "Backward" refers to characters highlighted *before* the cursor, i.e.
+;;   navigated via `F`.
+
+;;; Code:
+
 ;; TODO: Abort highlighting if before-cursor/after-cursor is too short.
 ;; TODO: Instead of just checking for the number of words, check for the number
 ;; of words *with accepted characters in them*, to determine when to start
